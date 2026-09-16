13336f4c43a99aa764a885b72bf4e264daf44936
JozwiakPiotr <piotrmaciej.j@gmail.com>
2026-09-16 22:36:43 +0200

    sso doc

---
+++ doc/SSO.md
@@ -0,0 +1,6 @@
+# Edge
+`Settings > System and performance` wyłączyć:
+- Background mode
+- Start-up boost
+
+uruchomić .\msedge.exe --auth-server-allowlist="*.contoso.lab" $url
--- tools/load.sh
+++ tools/load.sh
@@ -1,11 +1,13 @@
+# TODO: lepiej zrobić win jako prefix niż suffix np. winstart
+
 function virg() {
     for vm in $(virsh list --all --name | grep $2); do virsh $1 $vm; done
 }
 
-function startwin() {
+function winstart() {
     virg start win-
 }
 
-function stopwin() {
+function winstop() {
     virg shutdown win-
 }
